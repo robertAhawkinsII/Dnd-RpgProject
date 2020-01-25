e9c8877bab1ab3eaef6d01405ac21d963de6ae9a
robertAhawkinsII <60279727+robertAhawkinsII@users.noreply.github.com>
2020-01-24 22:13:16 -0500

    Add files via upload

---
+++ Assets/PreventClickDrag.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PreventClickDrag : ScrollRect
+{
+
+    public override void OnBeginDrag(PointerEventData eventData) { }
+    public override void OnDrag(PointerEventData eventData) { }
+    public override void OnEndDrag(PointerEventData eventData) { }
+}
